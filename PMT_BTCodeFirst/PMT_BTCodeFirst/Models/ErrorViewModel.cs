27857b14b546b38de5e5aa455b10d7786a852b32
PhanManhTung-347 <phanmanhtung24112006@gmail.com>
2026-04-10 09:05:17 +0700

    PhanManhTung_Step4_ConfigureDatabaseConnection

---
+++ PMT_BTCodeFirst/PMT_BTCodeFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PMT_BTCodeFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

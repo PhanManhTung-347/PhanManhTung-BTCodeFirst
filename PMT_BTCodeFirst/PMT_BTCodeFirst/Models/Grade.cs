b216b9530e114ec71c5457bb182f367afe4b4d50
PhanManhTung-347 <phanmanhtung24112006@gmail.com>
2026-04-10 09:14:02 +0700

    PhanManhTung_Step5_DangKyDbContext

---
+++ PMT_BTCodeFirst/PMT_BTCodeFirst/Models/Grade.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace PMT_BTCodeFirst.Models
+{
+    public class Grade
+    {
+        [Key]
+        public int GradeId { get; set; }
+        [Required]
+        public string GradeName { get; set; }
+        public virtual ICollection<Student> Students { get; set; }
+    }
+}

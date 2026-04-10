f41124b5ad5bb5699b21d572e49ffbc5d63554b3
PhanManhTung-347 <phanmanhtung24112006@gmail.com>
2026-04-10 09:37:18 +0700

    PhanManhTung_Step6_CreateMigration

---
+++ PMT_BTCodeFirst/PMT_BTCodeFirst/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PMT_BTCodeFirst.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Grade> Grades { get; set; }
+    public DbSet<Course> Courses { get; set; }
+    }
+}
---
+++ PMT_BTCodeFirst/PMT_BTCodeFirst/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace PMT_BTCodeFirst.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+        [Required]
+        public int GradeId { get; set; }
+
+        [ForeignKey("GradeId")]
+        public virtual Grade Grade { get; set; }
+        public virtual ICollection<Course> Courses { get; set; }
+    }
+}
